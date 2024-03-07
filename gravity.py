3e1ff7125b75a0a2288dc1d7dc370a6471d483de
Imanpal_158 <73012176+iman158@users.noreply.github.com>
2024-03-07 17:19:41 +0530

    Updated gravity.py
     completed celestial animation

--- gravity.py
+++ gravity.py
@@ -8,8 +8,6 @@ import pygame
 WIDTH = 800
 HEIGHT = 800
 CENTER = WIDTH // 2, HEIGHT // 2
-centerX = WIDTH // 2
-centerY = HEIGHT // 2
 
 G = 0.2
 M = 10e7
@@ -17,25 +15,24 @@ M = 10e7
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
-r0 = 10
+r0 = 10  # Define r0 here
 
 pygame.init()
 
 
-class Particle:
-    def __init__(self, x, y):
-        self.g = G
-        self.mass = 2
+class CelestialBody:
+    def __init__(self, x, y, mass=2, momentum_x=500, momentum_y=500, dt=0.001):
+        self.g = 0.2
+        self.mass = mass
         self.x = x
         self.y = y
-        self.momentum_x = 500
-        self.momentum_y = 500
-        self.dt = 0.001
+        self.momentum_x = momentum_x
+        self.momentum_y = momentum_y
+        self.dt = dt
 
-    def move(self, x_y_central_mass):
-        x2 = x_y_central_mass[0]
-        y2 = x_y_central_mass[1]
-        hyp = (self.x - x2) ** 2 + (self.y - y2) ** 2
+    def move(self, central_mass):
+        x2, y2 = central_mass
+        hyp = math.hypot(self.x - x2, self.y - y2)
         theta = math.atan2(y2 - self.y, x2 - self.x)
         force = (self.g * self.mass * M) / hyp
         force_x = force * math.cos(theta)
@@ -47,55 +44,40 @@ class Particle:
         return [self.x, self.y]
 
 
+def generator(shape='line'):
+    particles = []
+    if shape == 'line':
+        for i in range(1000):
+            x = randrange(-500, 1000)
+            y = 100
+            particles.append(CelestialBody(x, y))
+    elif shape == 'circle':
+        for i in range(1000):
+            ang = random.uniform(0, 1) * 2 * math.pi
+            hyp = sqrt(random.uniform(0, 1)) * r
+            adj = cos(ang) * hyp
+            opp = sin(ang) * hyp
+            x = CENTER[0] + adj
+            y = CENTER[1] + opp
+            particles.append(CelestialBody(x, y))
+    elif shape == 'square':
+        for i in range(500):
+            x = randrange(0, 500)
+            y = randrange(0, 500)
+            particles.append(CelestialBody(x, y))
+    return particles
+
+
+def draw(particles, screen):
+    for particle in particles:
+        pygame.draw.circle(screen, WHITE, (int(particle.move(CENTER)[0]), int(particle.move(CENTER)[1])), 1)
+
+
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-
-particles = []
-
-r = 200
-
-
-# Line
-def generator():
-    for i in range(1000):
-        x = randrange(-500, 1000)
-        y = 100
-        p = Particle(x, y)
-        particles.append(p)
-
-
-# Circle
-# def generator():
-#     for i in range(1000):
-#         ang = random.uniform(0, 1) * 2 * math.pi
-#         hyp = sqrt(random.uniform(0, 1)) * r
-#         adj = cos(ang) * hyp
-#         opp = sin(ang) * hyp
-#         x = centerX + adj
-#         y = centerY + opp
-#         p = Particle(x, y)
-#         particles.append(p)
-
-
-# Square
-# def generator():
-#     for i in range(500):
-#         x = randrange(0, 500)
-#         y = randrange(0, 500)
-#         p = Particle(x, y)
-#        particles.append(p)
-
-
-generator()
-
-
-def draw():
-    for i in range(len(particles)):
-        pygame.draw.circle(screen, WHITE, (particles[i].move(CENTER)), 1)
-
+particles = generator(shape='line')
 
 running = True
 while running:
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -103,8 +85,8 @@ while running:
     screen.fill(BLACK)
 
     # Gravity point
-    # central_mass = pygame.draw.circle(screen, WHITE, CENTER, r0)
+    pygame.draw.circle(screen, WHITE, CENTER, r0)
 
-    draw()
+    draw(particles, screen)
 
     pygame.display.update()
